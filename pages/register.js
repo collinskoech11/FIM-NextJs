9c3e48b57f4a1fc020ce07f69aed67d27f06f285
collinskoech11 <collinskoechck34@gmail.com>
2021-02-24 09:48:10 +0300

    react bootstrap update

---
+++ pages/register.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+export default () => (
+    <div>
+        <div>Register page here.</div>
+        <Link href="/">
+            <a>Home</a>
+        </Link>
+    </div>
+)
