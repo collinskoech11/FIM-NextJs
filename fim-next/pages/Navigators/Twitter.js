7bce9146d54205c6b5fdd14c9903ea444557a456
collinskoech11 <collinskoechck34@gmail.com>
2021-02-27 10:27:00 +0300

    update

--- fim-next/pages/Navigators/Twitter.js
+++ fim-next/pages/Navigators/Twitter.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './style.css';
 import { TwitterTimelineEmbed,  TwitterFollowButton } from 'react-twitter-embed';
 
 function Twitter() {
--- fim-next/pages/index.js
+++ fim-next/pages/index.js
@@ -1,13 +1,12 @@
-import styles from '../styles/Home.module.css'
 import {BrowserRouter as Router,Route} from 'react-router-dom';
-import Homepage from './pages/Homepage'
+import Navbar from './Navigators/Navbar';
+
 
 export default function Home() {
   return (
-    <>
     <Router>
-      <Route exact path="/" component={Homepage}/>
+      <Navbar/>
+      
     </Router>
-    </>
   )
 }
